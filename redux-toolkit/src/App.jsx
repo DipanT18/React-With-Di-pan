ab3bf74288b553013e08899e29d530bcece1512f
DipanT18 <dipantimalsina055@gmail.com>
2026-04-21 17:19:07 +0545

    React Fundamentals Course Comleted

---
+++ redux-toolkit/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Todos from './Components/Todos'
+import AddTodo from './Components/AddTodo'
+
+function App() {
+  return (
+    <>
+     <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
